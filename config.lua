ea0d368fc050156b1b569ae70f1abf455e85b715
Ilias Rbayti <128304168+5d-scripts@users.noreply.github.com>
2023-05-12 20:22:27 +0200

    v1.0.0

--- config.lua
+++ config.lua
@@ -2,18 +2,129 @@ _CONFIG =
 {
     locale = "en",
     bleedOutTimeSeconds = 900, -- Seconds until player is force-respawned.
-    respawnTimeSeconds = 55,  -- Second at which player can manually respawn. Can't be higher than bleedOutTime. Set to 0 to disable.
+    earlyRespawnTimeSeconds = 10, -- Second at which player can manually respawn. Can't be higher than bleedOutTime. Set to 0 to disable.
+    enableDeathCam = true, -- enable/disable fixed deathcam
+    enableDeathBlur = true, -- enable/disable motion blur on death
+
+    dyingAnimation = -- Play a dyning animation on player death
+    {
+        enabled = true,
+        dictionary = "combat@damage@rb_writhe",
+        animation = "rb_writhe_loop"
+    },
+
     Keys = -- Don't forget to also change the displayed keys in the UI.
     {
         dispatchKey = 47,
         syncKey = 303,
         respawnKey = 38
     },
+
     RespawnPoints = -- Will choose the closest respawn point.
     {
-        vector4(341.0, -1397.3, 32.5,48.5), -- Central Los Santos
-        vector4(1836.03, 3670.99, 34.28,296.06) -- Sandy Shores
+        vector4(341.0, -1397.3, 32.5, 48.5), -- Central Los Santos
+        vector4(1836.03, 3670.99, 34.28, 296.06) -- Sandy Shores
+    },
+
+    DeathCauseRules = -- Set rules based on players death cause. 
+    -- @deathCaueses: List of all death cause hashes that trigger the rule.
+    -- @modifiedBleedOutTime (optional): Set a different bleedout time for player.
+    -- @disallowDispatch: Disallow player from sending a dispatch if set to true.
+    -- @disallowEarlyRespawn: Disallow player from respawning early if set to true.
+    -- @allowRespawnAtPlaceOfDeath: Allow player to respawn at their place of death instead of the closest hospital if set to true. Allows them to keep their inventory.
+    {
+        deathByFallDamage =
+        {
+            deathCauses = {-842959696},
+            --modifiedBleedOutTime = 900,
+            disallowDispatch = false,
+            disallowEarlyRespawn = false,
+            allowRespawnAtPlaceOfDeath = false
+        },
+
+        deathByMelee = 
+        {
+            deathCauses = {-1569615261, 1737195953, 1317494643, -1786099057, 1141786504, -2067956739, -868994466},
+            --modifiedBleedOutTime = 900,
+            disallowDispatch = false,
+            disallowEarlyRespawn = false,
+            allowRespawnAtPlaceOfDeath = true
+        },
+
+        deathByKnife = 
+        {
+            deathCauses = {-1716189206, 1223143800, -1955384325, -1833087301, 910830060},
+            --modifiedBleedOutTime = 900,
+            disallowDispatch = false,
+            disallowEarlyRespawn = false,
+            allowRespawnAtPlaceOfDeath = false
+        },
+
+        deathByBullet = 
+        {
+            deathCauses = {453432689, 1593441988, 584646201, -1716589765, 324215364, 736523883, -270015777, -1074790547, -2084633992, -1357824103, -1660422300, 2144741730, 487013001, 2017895192,
+                           -494615257, -1654528753, 100416529, 205991906, 1119849093},
+            --modifiedBleedOutTime = 900,
+            disallowDispatch = false,
+            disallowEarlyRespawn = false,
+            allowRespawnAtPlaceOfDeath = false
+        },
+
+        deathByAnimal = 
+        {
+            deathCauses = {-100946242, 148160082},
+            --modifiedBleedOutTime = 900,
+            disallowDispatch = false,
+            disallowEarlyRespawn = false,
+            allowRespawnAtPlaceOfDeath = false
+        },
+
+        deathByExplosion = 
+        {
+            deathCauses = {-1568386805, 1305664598, -1312131151, 375527679, 324506233, 1752584910, -1813897027, 741814745, -37975472, 539292904, 341774354, -1090665087},
+            --modifiedBleedOutTime = 900,
+            disallowDispatch = false,
+            disallowEarlyRespawn = false,
+            allowRespawnAtPlaceOfDeath = false
+        },
+
+        deathByGas = 
+        {
+            deathCauses = {-1600701090},
+            --modifiedBleedOutTime = 900,
+            disallowDispatch = false,
+            disallowEarlyRespawn = false,
+            allowRespawnAtPlaceOfDeath = false
+        },
+
+        deathByBurn = 
+        {
+            deathCauses = {615608432, 883325847, -544306709},
+            --modifiedBleedOutTime = 900,
+            disallowDispatch = false,
+            disallowEarlyRespawn = false,
+            allowRespawnAtPlaceOfDeath = false
+        },
+
+        deathByDrown = 
+        {
+            deathCauses = {-10959621, 1936677264},
+            --modifiedBleedOutTime = 900,
+            disallowDispatch = false,
+            disallowEarlyRespawn = false,
+            allowRespawnAtPlaceOfDeath = false
+        },
+
+        deathByCar = 
+        {
+            deathCauses = {133987706, -1553120962},
+            --modifiedBleedOutTime = 900,
+            disallowDispatch = false,
+            disallowEarlyRespawn = false,
+            allowRespawnAtPlaceOfDeath = false
+        } 
     }
+
 }
 
 if (not IsDuplicityVersion()) then
